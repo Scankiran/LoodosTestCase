e27ed1ea6ff500d47254814324a7d872eb08f03c
Said ÇANKIRAN <cank32cank@gmail.com>
2022-03-24 12:33:46 +0300

    Project created.

---
+++ LoodosTestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosTestCase
+//
+//  Created by Said Çankıran on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
